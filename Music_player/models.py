58f7df8d281ad24ce15857ce41b8e580ac5f8bc0
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-30 02:46:53 +0530

    Update models.py

--- Music_player/models.py
+++ Music_player/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Album(models.Model):
     album_name=models.CharField(max_length=20)
-    album_image=models.FileField(upload_to="images"),
+    album_image=models.FileField(upload_to="images")
     
     def __str__(self) :
         return self.album_name
