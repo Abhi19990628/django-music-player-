62b91e31d1889376bb2a64f1c6bdf5be6385bf98
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-04-30 02:47:32 +0530

    Update models.py

--- Music_player/models.py
+++ Music_player/models.py
@@ -8,7 +8,7 @@ class Album(models.Model):
         return self.album_name
     
 class Songs(models.Model):
-    name=models.CharField(max_length=20),
+    name=models.CharField(max_length=20)
     album_name=models.ForeignKey(Album,on_delete=models.CASCADE)
     song=models.FileField(upload_to="song")
     picture=models.FileField(upload_to="picture")
